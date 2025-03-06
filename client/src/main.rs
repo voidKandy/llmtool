3274d521077b60fa7b91ad85dc497cb28bfc51d5
voidKandy <voidkandy@gmail.com>
2025-03-06 14:27:48 -0800

    cleaned up client

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,6 +1,8 @@
 use components::notes::{generate_mock_notes, NotesComponent, NotesProps, NotesPropsBuilder};
 use dioxus::prelude::*;
+// use model::get_model_and_load;
 mod components;
+// mod model;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
 const MAIN_CSS: Asset = asset!("/assets/main.css");
@@ -14,14 +16,27 @@ fn main() {
 fn App() -> Element {
     let mock_notes = generate_mock_notes();
     rsx! {
-        document::Script{src: asset!("/assets/bertWorker.js"), type: Some("module".to_string()) }
-        document::Script{src: asset!("/assets/utils.js"), type: Some("module".to_string()) }
-        document::Script{src: asset!("/assets/test.js"), type: Some("module".to_string()) }
+        // document::Script{src: asset!("/assets/bertWorker.js"), type: Some("module".to_string()) }
+        // document::Script{src: asset!("/assets/utils.js"), type: Some("module".to_string()) }
+        // document::Script{src: asset!("/assets/test.js"), type: Some("module".to_string()) }
         // Hero {}
         NotesComponent{notes: mock_notes }
+         // button {
+         //    onclick: move |_| async move {
+         //        get_model_and_load().await;
+         //        tracing::warn!("You clicked the button one second ago!");
+         //    },
+         //    "Click me"
+        // }
+
+        // button { onclick: move |_| async move{  get_model_and_load }, id:"click!", "click me"}
         // document::Link { rel: "icon", href: FAVICON }
         // document::Link { rel: "stylesheet", href: MAIN_CSS }
-        // Echo {}
+         // button {
+            // The `onclick` event accepts a closure with the signature `fn(Event)`
+            // onclick: |event_data| tracing::warn!("clicked! I got the event data: {event_data:?}"),
+            // "Click me",
+        // }
     }
 }
 
